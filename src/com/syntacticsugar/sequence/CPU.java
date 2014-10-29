9c02cada93da8f20ce8e5a63348e43f7608bb59d
Tommy <t0mmy@asu.edu>
2014-10-28 21:15:26 -0700

    sup

---
+++ src/com/syntacticsugar/sequence/CPU.java
@@ -0,0 +1,38 @@
+package com.syntacticsugar.sequence;
+
+public class CPU {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fuck you
--- src/com/syntacticsugar/sequence/Card.java
+++ src/com/syntacticsugar/sequence/Card.java
@@ -2,6 +2,9 @@ package com.syntacticsugar.sequence;
 
 public class Card {
 	
+	private string name;
+	private string suit;
+	
 	// hellow an welcum
 	// in this weekz java file
 	// we will b lrning how 2 make a card
