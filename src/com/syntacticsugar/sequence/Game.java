635579fede36d5cb1eaedf59431015fe5819a81e
Sean <sean.scott894@gmail.com>
2014-10-28 22:40:43 -0700

    Created Game

---
+++ src/com/syntacticsugar/sequence/Game.java
@@ -0,0 +1,17 @@
+package com.syntacticsugar.sequence;
+
+public class Game {
+
+	public static void main(String[] args) 
+	{
+		// Game starts
+		
+		// Initialize 2 Deck objects
+		
+		// Initialize a Board object
+		
+		// etc...
+
+	}
+
+}
