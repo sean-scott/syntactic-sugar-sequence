e79a811f55e7f1ee58b67fc92df6e2e2844ce47b
Tommy <t0mmy@asu.edu>
2014-10-28 22:30:48 -0700

    Deck now has Cards!

--- src/com/syntacticsugar/sequence/Deck.java
+++ src/com/syntacticsugar/sequence/Deck.java
@@ -8,11 +8,78 @@ public class Deck extends Card {
 
 public static void main(String[] args){
 	
-	deck[0] = new Card("Ace","Hearts"); // ayy nigga wat did i tell u
-	deck[0].printCard();
-
-
+	// Hearts
+	deck[0] = new Card("A", "Hearts");
+	deck[1] = new Card("2", "Hearts");
+	deck[2] = new Card("3", "Hearts");
+	deck[3] = new Card("4", "Hearts");
+	deck[4] = new Card("5", "Hearts");
+	deck[5] = new Card("6", "Hearts");
+	deck[6] = new Card("7", "Hearts");
+	deck[7] = new Card("8", "Hearts");
+	deck[8] = new Card("9", "Hearts");
+	deck[9] = new Card("10", "Hearts");
+	deck[10] = new Card("J", "Hearts");
+	deck[11] = new Card("Q", "Hearts");
+	deck[12] = new Card("K", "Hearts");
 	
+	// Diamonds
+	deck[13] = new Card("A", "Diamonds");
+	deck[14] = new Card("2", "Diamonds");
+	deck[15] = new Card("3", "Diamonds");
+	deck[16] = new Card("4", "Diamonds");
+	deck[17] = new Card("5", "Diamonds");
+	deck[18] = new Card("6", "Diamonds");
+	deck[19] = new Card("7", "Diamonds");
+	deck[20] = new Card("8", "Diamonds");
+	deck[21] = new Card("9", "Diamonds");
+	deck[22] = new Card("10", "Diamonds");
+	deck[23] = new Card("J", "Diamonds");
+	deck[24] = new Card("Q", "Diamonds");
+	deck[25] = new Card("K", "Diamonds");
+	
+	// Clubs
+	deck[26] = new Card("A", "Clubs");
+	deck[27] = new Card("2", "Clubs");
+	deck[28] = new Card("3", "Clubs");
+	deck[29] = new Card("4", "Clubs");
+	deck[30] = new Card("5", "Clubs");
+	deck[31] = new Card("6", "Clubs");
+	deck[32] = new Card("7", "Clubs");
+	deck[33] = new Card("8", "Clubs");
+	deck[34] = new Card("9", "Clubs");
+	deck[35] = new Card("10", "Clubs");
+	deck[36] = new Card("J", "Clubs");
+	deck[37] = new Card("Q", "Clubs");
+	deck[38] = new Card("K", "Clubs");
+	
+	// Spades
+	deck[39] = new Card("A", "Spades");
+	deck[40] = new Card("2", "Spades");
+	deck[41] = new Card("3", "Spades");
+	deck[42] = new Card("4", "Spades");
+	deck[43] = new Card("5", "Spades");
+	deck[44] = new Card("6", "Spades");
+	deck[45] = new Card("7", "Spades");
+	deck[46] = new Card("8", "Spades");
+	deck[47] = new Card("9", "Spades");
+	deck[48] = new Card("10", "Spades");
+	deck[49] = new Card("J", "Spades");
+	deck[50] = new Card("Q", "Spades");
+	deck[51] = new Card("K", "Spades");
+	
+	// Jokers
+	deck[52] = new Card("Joker", "One");
+	deck[53] = new Card("Joker", "Two");
+	
+	
+	
+	
+	
+	
+	
+	
+	// hey
 
 	
 	
