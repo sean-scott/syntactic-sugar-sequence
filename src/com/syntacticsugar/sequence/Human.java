d2adca73cb9db1ad3de7a02b66f9ce9206be10c8
Sean <sean.scott894@gmail.com>
2014-10-28 21:34:31 -0700

    rly?

--- src/com/syntacticsugar/sequence/Human.java
+++ src/com/syntacticsugar/sequence/Human.java
@@ -1,10 +1,5 @@
 package com.syntacticsugar.sequence;
 
 public class Human extends Player {
-	
-	// ayy girl lemme take u 2 da movies
 
-} // wut. This isn't something you'd write here... THOMAS.
-
-
-// Scott is here, writing some awesome code that will determine when Human will die.
+}
