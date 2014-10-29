1123312689d13a9ca9a024843e7e575793a0cf03
Tommy <t0mmy@asu.edu>
2014-10-28 21:29:22 -0700

    ...

--- src/com/syntacticsugar/sequence/Human.java
+++ src/com/syntacticsugar/sequence/Human.java
@@ -4,7 +4,7 @@ public class Human extends Player {
 	
 	// ayy girl lemme take u 2 da movies
 
-}
+} // ahoy bansal is best bansal
 
 
 // Scott is here, writing some awesome code that will determine when Human will die.
