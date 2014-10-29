1a5bff585bb3c2e0c7da9108acc00366e2c55eff
Sean <sean.scott894@gmail.com>
2014-10-28 21:09:28 -0700

    hellow again

--- src/com/syntacticsugar/sequence/Card.java
+++ src/com/syntacticsugar/sequence/Card.java
@@ -5,5 +5,7 @@ public class Card {
 	// hellow an welcum
 	// in this weekz java file
 	// we will b lrning how 2 make a card
+	
+	// It is a simple process, but you may want to speed it up to 1.5x
 
 }
