c63c49c653acc6205d21a155ae7cd96c2a9b8039
Aaron <alajom28@gmail.com>
2014-10-30 19:28:09 -0700

    Added the remove, select, and draw card methods under the player class. Not sure if the draw or select function are what we need...

--- src/com/syntacticsugar/sequence/Player.java
+++ src/com/syntacticsugar/sequence/Player.java
@@ -1,5 +1,7 @@
 package com.syntacticsugar.sequence;
 
+import java.util.Random;
+
 public class Player {
 	
 	Card[] hand = new Card[6];
@@ -17,6 +19,36 @@ public class Player {
 		
 		
 		
+	}
+	
+	public Card selectCard(int index){
+		return hand[index]; //Best I could do
+	}
+	
+	public void removeCard(int index){
+		hand[index].setName("null"); //Follows same principal as deck's remove method
+	}
+	
+
+	public void drawCard(){ 
+		
+		Random rand = new Random(System.currentTimeMillis());
+ 		
+ 		int min = 0; int max = 107;
+ 		
+ 		//Will find the removed card and replace it with a random card from the deck
+		for(int j = 0; j < 6; j++){ 
+			if(hand[j].getName() == "null"){ 
+				
+				int randomCard = rand.nextInt(max + 1) + min;
+				
+				//Copied from the generate hand method to pull from the deck
+	 			if (!Deck.deck[randomCard].getName().equals("null")){
+	 				hand[j] = Deck.deck[randomCard];
+	 				removeCard(randomCard);
+	 			}
+			}
+		}
 	}
 	
 	// hellowwwww... I hit my elbow
